a60f1fa842e3faabe1381d1cf4f4ab59772e718e
Ian Molyneaux <ianmolyneaux92@gmail.com>
2026-01-01 20:57:21 +0000

    Updated app.component so Favourites and Settings Icons are now visible on the Nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
+import { addIcons } from 'ionicons';
+import { heartOutline, settingsOutline } from 'ionicons/icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({
+      'heart-outline': heartOutline,
+      'settings-outline': settingsOutline,
+    });
+  }
 }
